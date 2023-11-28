fdc22dc36fd63a2a35cd2097e33e7cd699400cae
sagardwivedi <sagar.dw20@gmail.com>
2023-11-28 15:39:55 +0530

    feat: sidebar show and hide complete

--- app/ui/shownovchanger.tsx
+++ app/ui/shownovchanger.tsx
@@ -1,10 +1,29 @@
-import { EyeSlashIcon } from '@heroicons/react/24/outline';
+'use client';
+
+import { useSidebarHide } from '@/hooks/useSidebarHide';
+import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/outline';
 
 export function ShowNavChanger() {
+  const { onClose, isOpen, onOpen } = useSidebarHide();
+
   return (
-    <div className="mt-4 flex flex-row items-center gap-3 text-sm text-white/50">
-      <EyeSlashIcon className="h-5 w-5" />
-      Hide Sidebar
-    </div>
+    <>
+      {isOpen ? (
+        <div
+          onClick={onClose}
+          className="mt-4 flex cursor-pointer flex-row items-center gap-3 text-sm text-white/50"
+        >
+          <EyeSlashIcon className="h-5 w-5" />
+          Hide Sidebar
+        </div>
+      ) : (
+        <div
+          className="absolute bottom-2 left-0 cursor-pointer rounded-r-full bg-neutral-950 p-2"
+          onClick={onOpen}
+        >
+          <EyeIcon className="h-5 w-5" />
+        </div>
+      )}
+    </>
   );
 }
--- app/ui/side-nav.tsx
+++ app/ui/side-nav.tsx
@@ -1,11 +1,20 @@
+'use client';
+
+import { useSidebarHide } from '@/hooks/useSidebarHide';
 import { Logo } from './logo';
 import { Nav } from './nav';
 import { ShowNavChanger } from './shownovchanger';
 import { ThemeChanger } from './theme-changer';
 
 export function SideNav() {
+  const { isOpen } = useSidebarHide();
+
   return (
-    <div className="h-full w-64 border-r border-white/20 bg-primary-background">
+    <div
+      className={`h-full ${
+        isOpen ? 'w-64' : 'w-0'
+      } border-r border-white/20 bg-primary-background transition-all duration-100`}
+    >
       <div className="flex h-full flex-col justify-between">
         <div>
           <div className="h-20 pl-8 pt-5 ">
